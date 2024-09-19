0d898d2a4c6c25a6abb940da4acaf1036e0c5eb4
fafase <lucascoss@gmail.com>
2024-09-19 17:48:09 +0300

    Added paddle constrains

--- Assets/Scripts/Paddle/PaddleController.cs
+++ Assets/Scripts/Paddle/PaddleController.cs
@@ -5,18 +5,46 @@ public class PaddleController
 {
     private float m_speed;
     private Vector3 m_defaultPosition;
+    private Vector2 m_limits;
+
+    private readonly float m_defaultSize;
+    private readonly IPaddleProvider m_provider;
+    private readonly float m_deckLeft, m_deckRight;
 
     public ReactiveProperty<Vector3> PaddlePos = new ReactiveProperty<Vector3>();
-    public PaddleController(Vector3 defaultPos)
+    public ReactiveProperty<float> PaddleScale = new ReactiveProperty<float>();
+
+    public PaddleController(IPaddleProvider provider, Vector3 defaultPos)
     {
+        m_provider = provider;
+        m_deckLeft = Camera.main.WorldToScreenPoint(m_provider.XLeft).x;
+        m_deckRight = Camera.main.WorldToScreenPoint(m_provider.XRight).x;
+
         m_defaultPosition = defaultPos;
+        PaddleScale.Value =  m_provider.Scale;
+        PaddleScale
+            .DelayFrame(1)
+            .Subscribe(SetLimits);
+        SetLimits(0f);
     }
 
     public void ProcessPosition(Vector3 mousePos)
     {
-        Debug.Log($"Start {mousePos}");
-        mousePos.x = Mathf.Clamp(mousePos.x, 0, Screen.width);
+        mousePos.x = Mathf.Clamp(mousePos.x, m_limits.x, m_limits.y);
         var xPos = Camera.main.ScreenToWorldPoint(mousePos);
         PaddlePos.Value = new Vector3(xPos.x, m_defaultPosition.y, m_defaultPosition.z);
+    }
+
+    public void SetScale(long obj) => PaddleScale.Value = (PaddleScale.Value > 2.5f) ? 2f : 3f;
+
+    private void SetLimits(float newScale) 
+    {
+        float paddleHalfWidthWorld = m_provider.Size / 2f;
+        Vector3 paddleScreenSpace = Camera.main.WorldToScreenPoint(new Vector3(paddleHalfWidthWorld, 0, 0));
+        float paddleHalfWidthScreen = paddleScreenSpace.x - Camera.main.WorldToScreenPoint(Vector3.zero).x;
+
+        float adjustedDeckLeft = m_deckLeft + paddleHalfWidthScreen;
+        float adjustedDeckRight = m_deckRight - paddleHalfWidthScreen;
+        m_limits = new Vector2(adjustedDeckLeft, adjustedDeckRight);
     }
 }
--- Assets/Scripts/Paddle/PaddlePresenter.cs
+++ Assets/Scripts/Paddle/PaddlePresenter.cs
@@ -1,14 +1,29 @@
 using UniRx;
 using UnityEngine;
 
-public class PaddleView : MonoBehaviour
+public class PaddleView : MonoBehaviour, IPaddleProvider
 {
     [SerializeField] private Transform m_paddleTr;
+    [SerializeField] private Transform m_deckLeftLimit, m_deckRightLimit;
+    [SerializeField] private Transform m_paddleLeftLimit, m_paddleRightLimit;
+
     private PaddleController m_paddleController;
+
+    public Vector3 XLeft => m_deckLeftLimit.position;
+    public Vector3 XRight => m_deckRightLimit.position;
+    public float Size => GetComponent<SpriteRenderer>().bounds.size.x;
+    public float Scale => transform.localScale.x;
 
     private void Start()
     {
-        m_paddleController = new(m_paddleTr.position);
+        SetPaddleController();
+        SetUpdate();
+    }
+
+    private void SetPaddleController() 
+    {
+        SpriteRenderer sp = m_paddleTr.GetComponent<SpriteRenderer>();
+        m_paddleController = new(this, m_paddleTr.position);
         m_paddleController.PaddlePos
             .Skip(1)
             .Subscribe(newPosition =>
@@ -16,11 +31,34 @@ public class PaddleView : MonoBehaviour
                 m_paddleTr.position = newPosition;
             })
             .AddTo(this);
+        m_paddleController.PaddleScale
+            .Subscribe(newScale =>
+            {
+                Vector3 scale = m_paddleTr.localScale;
+                m_paddleTr.localScale = new Vector3(newScale, scale.y, scale.z);
+            });
+    }
 
-         Observable.EveryUpdate()
+    private void SetUpdate() 
+    {
+        var update = Observable.EveryUpdate();
+        update
             .Where(_ => Input.GetMouseButton(0))
             .Select(_ => Input.mousePosition)
             .Subscribe(m_paddleController.ProcessPosition)
             .AddTo(this);
+
+        update
+            .Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(m_paddleController.SetScale);
     }
+}
+
+public interface IPaddleProvider 
+{
+    Vector3 XLeft { get; }
+    Vector3 XRight { get; }
+    float Size { get; }
+    float Scale { get; }
+    
 }
