676d289f65879e52118fa7593bb6b269e8183562
fafase <lucascoss@gmail.com>
2024-10-02 12:41:20 +0300

    UserPrefs

---
+++ Assets/Tools/Helpers/JsonUtility.cs
@@ -0,0 +1,40 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Tools
+{
+    public static class JsonUtility
+    {
+        // https://stackoverflow.com/questions/14977848/how-to-make-sure-that-string-is-valid-json-using-json-net
+        public static bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput)) { return false; }
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    //Exception in parsing json
+                    Console.WriteLine(jex.Message);
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/Tools/UserPrefs/UserPrefs.cs
@@ -0,0 +1,264 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+using UnityEditor;
+using UnityEngine;
+using Zenject;
+
+using Unity.Services.CloudSave;
+
+
+namespace Tools
+{
+    public class UserPrefs : IUserPrefs, IInitializable
+    {
+        private const string UserPrefsKey = "UserPrefsKey";
+        private JObject m_jsonObject;
+        public bool IsDirty { get; set; }
+        public bool IsInit { get; private set; }
+        public string Json => m_jsonObject.ToString();
+
+        public event Action OnUpdate;
+
+        public void Initialize()
+        {
+            string pp = PlayerPrefs.GetString(UserPrefsKey, "{}");
+            m_jsonObject = JObject.Parse(pp);
+            IsInit = true;
+        }
+
+        private bool GetValue<T>(string key, out JToken token)
+        {
+            token = null;
+            if (string.IsNullOrEmpty(key))
+            {
+                return false;
+            }
+            if (!IsInit)
+            {
+                Debug.LogError("[UserPrefs] Attempt to access UserPrefs while not initialized");
+                return false;
+            }
+            if (m_jsonObject == null)
+            {
+                return false;
+            }
+            return m_jsonObject.TryGetValue(key, out token);
+        }
+
+        public bool TryGetInt(string key, out int value, int defaultValue = 0)
+        {
+            if (GetValue<int>(key, out JToken token))
+            {
+                value = token.ToObject<int>();
+                return true;
+            }
+            value = defaultValue;
+            return false;
+        }
+        public bool TryGetFloat(string key, out float value, float defaultValue = 0.0f)
+        {
+            if (GetValue<float>(key, out JToken token))
+            {
+                value = token.ToObject<float>();
+                return true;
+            }
+            value = defaultValue;
+            return false;
+        }
+        public bool TryGetDate(string key, out DateTime value, DateTime defaultValue = default(DateTime))
+        {
+            if (GetValue<float>(key, out JToken token))
+            {
+                value = DateTime.Parse(token.ToString(), Thread.CurrentThread.CurrentCulture);
+                return true;
+            }
+            value = defaultValue;
+            return false;
+        }
+        public bool TryGetBool(string key, out bool value, bool defaultValue = false)
+        {
+            if (GetValue<bool>(key, out JToken token))
+            {
+                value = token.ToObject<bool>();
+                return true;
+            }
+            value = defaultValue;
+            return false;
+        }
+        public bool TryGetObject<T>(string key, out T value, T defaultValue) where T : class
+        {
+            value = defaultValue;
+            if (string.IsNullOrEmpty(key))
+            {
+                return false;
+            }
+            if (!IsInit)
+            {
+                Debug.LogError("[UserPrefs] Attempt to access UserPrefs while not initialized");
+                return false;
+            }
+            if (m_jsonObject == null)
+            {
+                return false;
+            }
+            if (m_jsonObject.TryGetValue(key, out JToken jtoken))
+            {
+                value = jtoken.ToObject<T>();
+                return true;
+            }
+            return false;
+        }
+        public void SetValue(string key, object value)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new System.ArgumentNullException("Provided key is null or empty while setting value");
+            }
+            m_jsonObject[key] = JToken.FromObject(value);
+            SaveLocal();
+            OnUpdate?.Invoke();
+        }
+
+        public bool RemoveKey(string key) => m_jsonObject.Remove(key);
+
+        public void ClearUserPrefs()
+        {
+            if (!IsInit)
+            {
+                return;
+            }
+            PlayerPrefs.DeleteKey(UserPrefsKey);
+            m_jsonObject = JObject.Parse("{}");
+            SaveLocal();
+        }
+
+
+        public bool SetUserPrefsFromRemote(string remoteUserPrefs)
+        {
+            if (!JsonUtility.IsValidJson(remoteUserPrefs))
+            {
+                Debug.LogError("[UserPrefs] Invalid Json from remote");
+                return false;
+            }
+
+            m_jsonObject = JObject.Parse(remoteUserPrefs);
+
+            SaveLocal();
+            return true;
+        }
+
+        public bool MergeContentFromRemote(string remoteUserPrefs)
+        {
+            if (!JsonUtility.IsValidJson(remoteUserPrefs))
+            {
+                Debug.LogError("[UserPrefs] Invalid Json from remote");
+                return false;
+            }
+            JObject remote = JObject.Parse(remoteUserPrefs);
+            m_jsonObject.Merge(remote, new JsonMergeSettings
+            {
+                // union array values together to avoid duplicates
+                MergeArrayHandling = MergeArrayHandling.Union
+            });
+            SaveLocal();
+            return true;
+        }
+
+        private void SaveLocal()
+        {
+            PlayerPrefs.SetString(UserPrefsKey, m_jsonObject.ToString());
+            IsDirty = true;
+        }
+
+#if UNITY_EDITOR
+        [MenuItem("Tools/Delete User prefs")]
+        static async void DoSomething()
+        {
+            IUserPrefs up = new UserPrefs();
+            ((IInitializable)up).Initialize();
+            up.ClearUserPrefs();
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Application needs to be running to save on cloud");
+                return;
+            }
+            await CloudSaveService.Instance.Data.Player.SaveAsync(new Dictionary<string, object>
+            {
+                { "playerData", up.Json }
+            });
+        }
+#endif
+    }
+
+    public interface IUserPrefs
+    {
+        /// <summary>
+        /// Indicates if a change was made to the content
+        /// </summary>
+        bool IsDirty { get; set; }
+
+        /// <summary>
+        /// Try and get the value matching the key. If the key exists, returns true and stored value is placed in value out parameter.
+        /// If the key does not exists, returns false and defaultValue is placed in value out parameter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">Key to be found in the stored content</param>
+        /// <param name="value">Updated value if key is found, else value is defaultValue</param>
+        /// <param name="defaultValue">Default value if key is not found</param>
+        /// <returns>True if the value is found, else false</returns>
+        bool TryGetObject<T>(string key, out T value, T defaultValue = null) where T : class;
+
+        bool TryGetInt(string key, out int value, int defaultValue = 0);
+        bool TryGetFloat(string key, out float value, float defaultValue = 0.0f);
+        bool TryGetDate(string key, out DateTime value, DateTime defaultValue = default(DateTime));
+        bool TryGetBool(string key, out bool value, bool defaultValue = false);
+
+        /// <summary>
+        /// Update the value from the key. 
+        /// Key/Value added if not stored
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void SetValue(string key, object value);
+
+        /// <summary>
+        /// Removes the key from the stored content
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>True if key is found and removed, false if the key does not exist</returns>
+        bool RemoveKey(string key);
+        /// <summary>
+        /// Clears the stored content and reset a default empty json object
+        /// </summary>
+        void ClearUserPrefs();
+        /// <summary>
+        /// Sets the stored content with the remote content. 
+        /// </summary>
+        /// <param name="remoteUserPrefs"></param>
+        /// <returns>True if the remote content is valid json and local content is updated, else false</returns>
+        bool SetUserPrefsFromRemote(string remoteUserPrefs);
+
+        /// <summary>
+        /// Merge remote json file with local json file. 
+        /// If a key exists in both files, remote file value is stored
+        /// </summary>
+        /// <param name="remoteUserPrefs"></param>
+        /// <returns>True if the remote content is valid json and local content is updated, else false</returns>
+        bool MergeContentFromRemote(string remoteUserPrefs);
+
+        /// <summary>
+        /// Json string of the UserPrefs object
+        /// </summary>
+        string Json { get; }
+
+        event Action OnUpdate;
+    }
+
+    public class UserPrefsUpdate : SignalData
+    {
+        public string json;
+        public UserPrefsUpdate(string json) => this.json = json;
+    }
+}
